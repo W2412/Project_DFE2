d5c126f3fe0425b55c80ec83d0fb8e4cc168fcaa
Your Name <razerzaaa@gmail.com>
2025-02-17 15:21:39 +0700

    Properties k and Cp Update 1

--- Properties/EXCEL.py
+++ Properties/EXCEL.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Define the path to your text file
-txt_file = "HFL1_RVE_CON_12.txt"  # Replace with your file path
+txt_file = "TEMP_CP_DFE2_23_X.txt"  # Replace with your file path
 
 # Read the text file into a DataFrame.
 # In this example, we assume the first two lines are headers that we want to skip.
@@ -11,9 +11,9 @@ df = pd.read_csv(txt_file, delim_whitespace=True, skiprows=2, header=None)
 df.columns = ["Part_Instance", "Node_ID", "Attached_elements", "HFL_HFL1"]
 
 # Define the output Excel file name
-excel_file = "output.xlsx"
+excel_file = "CP23_X.xlsx"
 
 # Write the DataFrame to an Excel file.
 df.to_excel(excel_file, index=False)
 
-print("Data has been extracted and saved to", excel_file)     
+print("Data has been extracted and saved to", excel_file)
